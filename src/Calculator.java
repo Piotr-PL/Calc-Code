3418e54143576e462015be1674c5768a6204f163
Piotr-PL <66646407+Piotr-PL@users.noreply.github.com>
2021-11-22 09:56:02 +0100

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -30,64 +30,77 @@ public class Calculator
             value.next();
             value2 = value.nextDouble();
         }
-
-
-        System.out.println("Chose the mark: \n\n+    -   *   \n/    %   ^    \np   s   c");
+        System.out.println("Chose the action type: \n\n1. Add           2. Substraction          3. Multiplication   \n4. Divide        5. Rest of the divide    6. Power    \n7. Square root   8. Sine                  9. Cosine");
 
         mark = value.next().charAt(0);  // Getting the mark from User
-        switch(mark)     // Do the operatrion from getting mark
+        switch (mark)     // Do the operation from getting mark
         {
-            case '+':    // Add
+            case '1':    // Add
             {
                 System.out.println("The result of the action is = " + (value1 + value2));
                 break;
             }
-            case '-':    // Subtraction
+            case '2':    // Subtraction
             {
                 System.out.println("The result of the action is = " + (value1 - value2));
                 break;
             }
-            case '*':    // Multiplication
+            case '3':    // Multiplication
             {
                 System.out.println("The result of the action is = " + (value1 * value2));
                 break;
             }
-            case '/':   // Divide
+            case '4':   // Divide
             {
-                if(!value2.equals(value1) && value2 !=0)  //Checking if the entered value isn't 0
-                {
-                    System.out.println("The result of the action is = " + (value1 / value2) + "\n");
-                }
-                else
-                {
-                    System.out.println("Don't divide by the zero !!!");
-                }
-                break;
+                if (value1 == 0)  //Checking if the entered value isn't 0
+                    {
+                        System.out.println("Don't divide by the zero !!!");
+                        System.out.println("Enter first value again:");
+                        value1 = value.nextDouble();
+                    }
+                    if (value2 == 0)
+                        {
+                            System.out.println("Don't divide by the zero !!!");
+                            System.out.println("Enter second value again:");
+                            value.nextDouble();
+                        }
+                        else
+                            {
+                                System.out.println("The result of the action is = " + (value1 / value2) + "\n");
+                            }break;
             }
-            case '%':
+            case '5':
             {
-                if(value2 !=0)  // Checking condition of divide by zero
-                {
-                    System.out.println("The result of the action is = " + (value1 % value2));
-                }
-                else
-                {
-                    System.out.println("Don't divide by the zero !!!");
-                }
+                if (value1 == 0)  //Checking if the entered value isn't 0
+                    {
+                        System.out.println("Don't divide by the zero !!!");
+                        System.out.println("Enter first value again:");
+                        value1 = value.nextDouble();
+                    }
+                    if (value2 == 0)
+                        {
+                            System.out.println("Don't divide by the zero !!!");
+                            System.out.println("Enter second value again:");
+                            value.nextDouble();
+                        }
+                        else
+                        {
+                        System.out.println("The result of the action is = " + (value1 % value2));
+                        } break;
             }
-            case '^':    // Power v1^v2
+            case '6':    // Power v1^v2
             {
                 double power = pow(value1, value2);
                 System.out.println("The result of the action is = " + (power));
                 break;
             }
-            case 'p':    // Sqrt = sqrtV1 + sqrtV2
+            case '7':    // Sqrt = sqrtV1 + sqrtV2
             {
-                double sqrt = sqrt(value1)+sqrt(value2);
-                System.out.println("The result of the action is = " + (sqrt));
-                break;
+                    double sqrt = sqrt(value1) + sqrt(value2);
+                    System.out.println("The result of the action is = " + (sqrt));
+                    break;
             }
-            case 's':    // Sine of value1 and value2 in radians
+            case '8':    // Sine of value1 and value2 in radians
             {
                 double radians1 = toRadians(value1);
                 double radians2 = toRadians(value2);
@@ -95,7 +108,7 @@ public class Calculator
                 System.out.println("The Sine of " + value2 + " degrees is  " + sin(radians2));
                 break;
             }
-            case 'c':    // Cosine of value1 and value2 in radians
+            case '9':    // Cosine of value1 and value2 in radians
             {
                 double radians1 = toRadians(value1);
                 double radians2 = toRadians(value2);
@@ -103,6 +116,9 @@ public class Calculator
                 System.out.println("The Cosine of " + value2 + " degrees is  " + cos(radians2));
                 break;
             }
-        }
+                default:
+                System.out.println("Chose the correct character ");
+                break;
+       }     
     }
 }
